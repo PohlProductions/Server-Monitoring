a4bd7e13b3553c482aa6a85015f869ea3db15cc7
Oliver Pohl <oliver1002pohl@freenet.de>
2024-07-20 17:20:24 +0200

    Append to logfile instead of replacing it

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ sed -i "s/UseSTARTTLS=YES/UseSTARTTLS=$UseSTARTTLS/g" /etc/ssmtp/ssmtp.conf
 echo "root:$FROM:$SMTPSERVER" >> /etc/ssmtp/revaliases
 echo "monitoring:$FROM:$SMTPSERVER" >> /etc/ssmtp/revaliases
 
-crontab -l | { cat; echo "*/1 * * * * /monitoring/monitoring.sh $REMOTE_HOST $SMTPSERVER $FROM $TO $USER $PASSWORD &> /var/log/monitoring/monitoring.log"; } | crontab -
+crontab -l | { cat; echo "*/1 * * * * /monitoring/monitoring.sh $REMOTE_HOST $SMTPSERVER $FROM $TO $USER $PASSWORD >> /var/log/monitoring/monitoring.log"; } | crontab -
 
 crond -f
